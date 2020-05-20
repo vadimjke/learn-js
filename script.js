71c2df1085169fbf07abdb53a39e34ab1605367d
vadimjke <it.vadik@gmail.com>
2020-05-20 11:55:28 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+
+// single comment
+
+
+// var leftBorderWidth = 1;
+// {
+//     let second = 2;
+//     console.log(second);
+// }
+// const pi = 3.14;
+
+/* 
+multiple
+comments
+*/
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[0]);
+
+let ans = +prompt("yo", "");
