08b2eab6351a7788ce2d8b96d4135dc9d7175a16
vadimjke <it.vadik@gmail.com>
2020-05-29 10:30:07 +0300

    Задание 10 (таймер)

--- Часть 2/project 2/js/script.js
+++ Часть 2/project 2/js/script.js
@@ -1,3 +1,6 @@
+// Табы
+
+
 // window.addEventListener('load');
 window.addEventListener('DOMContentLoaded', function() {
     'use strict';
@@ -33,6 +36,67 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+
+
+
+
+
+
+
+    
+// Таймер
+
+    let deadline = '2020-05-30';
+
+    function getTimeRemaining(endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/1000/60/60));
+            // hours = Math.floor((t/1000/60/60) % 24),
+            // days = Math.floor((t/1000*60*60*24));
+
+            if (seconds < 10) {seconds = '0'+seconds;}
+            if (minutes < 10) {minutes = '0'+minutes;}
+            if (hours < 10) {hours = '0'+hours;}
+
+            return {
+                'total': t,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds
+            };
+    }
+
+    
+    function setClock(id, endTime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeinterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endTime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeinterval);
+            }
+        }
+    }
+
+    setClock('timer', deadline);
+
+
+
+
+
+
+
 
 
 });
