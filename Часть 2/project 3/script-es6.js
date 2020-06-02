fe11f830e19df9c27c821c1a78253e3fef51c1d9
vadimjke <it.vadik@gmail.com>
2020-06-02 07:43:50 +0300

    ES6 spread оператор

--- Часть 2/project 3/script-es6.js
+++ Часть 2/project 3/script-es6.js
@@ -41,3 +41,27 @@ console.log (squareTwo.calcArea());
 
 
 
+
+
+// Spread операторы
+
+
+let video = ['youtube', 'vimeo', 'rutube'],
+    blogs = ['wordpress', 'livejournal', 'blogger'],
+    internet = [...video, ...blogs, 'vk', 'facebooks'];
+
+console.log(internet);
+
+
+function log(a, b, c) {
+    //
+    console.log(a);
+    console.log(b);
+    console.log(c);
+};
+
+let numbers = [2, 5, 6];
+
+log(...numbers);
+
+
