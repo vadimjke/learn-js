43a06d572bfdffdd2c03d4b6b1243055a0351097
vadimjke <it.vadik@gmail.com>
2020-06-02 09:50:51 +0300

    JSON и AJAX

---
+++ Часть 3/project-1/script.js
@@ -0,0 +1,11 @@
+let options = {
+    width: 1366,
+    height: 750,
+    background: 'red',
+    font: {
+        size: '16px',
+        color: '#fff'
+    }
+};
+
+console.log(JSON.parse(JSON.stringify(options)));
