40d695384f9cdf35315ad8a37bbd329083268eed
vadimjke <it.vadik@gmail.com>
2020-06-10 16:45:11 +0300

    Promise

---
+++ Часть 4/project-1/promise.js
@@ -0,0 +1,37 @@
+let drink = 1;
+
+
+function shoot(arrow) {
+    console.log('Выстрел был произведен');
+    let promise = new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            Math.random() > .5 ? resolve({}) : reject("Вы промахнулись");
+        }, 3000);
+    });
+
+    return promise;
+
+
+}
+
+function win() {
+    console.log('Вы победили');
+    (drink == 1) ? buyBeer() : giveMoney();
+}
+
+function buyBeer() {
+    console.log("Вам купили пиво");
+}
+
+function giveMoney() {
+    console.log('Вам дали денег');
+}
+
+function lose() {
+    console.log('Вы проиграли');
+}
+
+shoot({})
+    .then(mark => console.log('Вы попали в цель'))
+    .then(win)
+    .catch(lose);
---
+++ Часть 4/project-1/script.js
@@ -0,0 +1,37 @@
+let drink = 0;
+
+
+function shoot(arrow, headshot, fail) {
+    console.log('Выстрел был произведен');
+
+    setTimeout(function() {
+        Math.random() > .5 ? headshot({}) : fail("Вы промахнулись");
+    }, 3000);
+}
+
+function win() {
+    console.log('Вы победили');
+    (drink == 1) ? buyBeer() : giveMoney();
+}
+
+function buyBeer() {
+    console.log("Вам купили пиво");
+}
+
+function giveMoney() {
+    console.log('Вам дали денег');
+}
+
+function lose() {
+    console.log('Вы проиграли');
+}
+
+shoot({}, 
+    function(mark) {
+        console.log('Вы попали в цель!');
+        win(mark, buyBeer, giveMoney);
+    },
+    function(miss) {
+        console.error(miss);
+        lose();
+    });
