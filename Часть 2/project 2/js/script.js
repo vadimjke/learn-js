a9686016080fd2b9c2f51c198072d3642b782962
vadimjke <it.vadik@gmail.com>
2020-06-10 15:57:01 +0300

    Задание №13 - JSON и AJAX

--- Часть 2/project 2/js/script.js
+++ Часть 2/project 2/js/script.js
@@ -189,4 +189,60 @@ window.addEventListener('DOMContentLoaded', function() {
 
 
 
+
+
+
+
+        // Контактная форма
+
+        let contactMessage = {
+            loading : "Идет загрузка...",
+            success : "Успешно!",
+            failure : "что-то пощло не так"
+        };
+
+        let contactForm = document.querySelector('#form'),
+            contactInput = contactForm.getElementsByTagName('input'),
+            contactStatus = document.querySelector('.contact-form-title');
+
+        contactForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+            let request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-Type', 'applications/json; charset=utf-8');
+            let cFormData = new FormData(contactForm);
+            let obj = {
+                mail: contactInput[0].value,
+                phone: contactInput[1].value
+            };
+
+            let json = JSON.stringify(obj);
+            request.send(json);
+            console.log(json);
+            
+
+
+            request.addEventListener('readystatechange', function(){
+                if (request.readyState < 4) {
+                    contactStatus.innerHTML = message.loading;
+                } else if (request.readyState == 4 && request.status == 200) {
+                    contactStatus.innerHTML = message.success;
+                } 
+                else {
+                    contactStatus.innerHTML = message.failure;
+                }
+            });
+
+            for (let i = 0; i < contactInput.length; i++) {
+                contactInput[i].value = '';
+            }
+
+        });
+
+        
+
+
+
+
+
 });
